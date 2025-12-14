3d978ba0c68ad3eddcbcfcf83f921d56468e1431
sajad <s.eskandarian73@yahoo.com>
2025-12-14 11:53:45 +0330

    add filed date to order resources

--- src/Enums/VerificationMode.php
+++ src/Enums/VerificationMode.php
@@ -11,6 +11,7 @@ enum VerificationMode: string
     case CARD = 'card';
     case PASSWORD = 'password';
     case PHOTO = 'photo';
+    case QR_CODE = 'qr_code';
     case UNKNOWN = 'unknown';
 
     /** Create from device mode value */
@@ -21,6 +22,7 @@ enum VerificationMode: string
             $mode === 3 => self::CARD,       // checked
             $mode === 1 => self::FINGERPRINT, // checked
             $mode === 10 => self::PASSWORD,
+            $mode === 13 => self::QR_CODE, // checked
             $mode === 50 => self::PHOTO,
             default => self::UNKNOWN,
         };
@@ -35,6 +37,7 @@ enum VerificationMode: string
             self::CARD => 'Card',
             self::PASSWORD => 'Password',
             self::PHOTO => 'Photo',
+            self::QR_CODE => 'QR Code',
             self::UNKNOWN => 'Unknown',
         };
     }
